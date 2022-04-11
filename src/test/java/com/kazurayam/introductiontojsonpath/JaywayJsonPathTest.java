81af2baf577f44b86561d763581c4e20b92cb9a2
kazurayam <kazuaki.urayama@gmail.com>
2022-04-11 10:41:43 +0900

    4.2 Predicates

--- src/test/java/com/kazurayam/introductiontojsonpath/JaywayJsonPathTest.java
+++ src/test/java/com/kazurayam/introductiontojsonpath/JaywayJsonPathTest.java
@@ -1,6 +1,8 @@
 package com.kazurayam.introductiontojsonpath;
 
+import com.jayway.jsonpath.Criteria;
 import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.Filter;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,8 +12,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,30 +24,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class JaywayJsonPathTest {
 
-    private static final Path dataFile = Paths.get("./src/test/fixture/data01.json");
-    private static String jsonDataSourceString;
-
-    private String jsonpathCreatorNamePath = "$['tool']['jsonpath']['creator']['name']";
-    private String jsonpathCreatorLocationPath = "$['tool']['jsonpath']['creator']['location'][*]";
-
-    @BeforeAll
-    public static void beforeAll() throws IOException {
+    String readData(Path dataFile) throws IOException {
         List<String> allLines = Files.readAllLines(dataFile);
-        jsonDataSourceString = allLines.stream()
+        return allLines.stream()
                 .collect(Collectors.joining(System.lineSeparator()));
     }
 
     @Test
-    public void test0() {
+    public void test0() throws IOException {
+        Path dataFile = Paths.get("./src/test/fixture/data01.json");
+        String jsonDataSourceString = readData(dataFile);
         System.out.println(jsonDataSourceString);
     }
 
     @Test
-    public void test4_1_AccessToDocuments() {
+    public void test4_1_AccessToDocuments() throws IOException {
+        Path dataFile = Paths.get("./src/test/fixture/data01.json");
+        String jsonDataSourceString = readData(dataFile);
         DocumentContext jsonContext = JsonPath.parse(jsonDataSourceString);
-        String jsonpathCreatorName = jsonContext.read(jsonpathCreatorNamePath);
-        List<String> jsonpathCreatorLocation = jsonContext.read(jsonpathCreatorLocationPath);
+        //
+        String jsonpathCreatorName =
+                jsonContext.read("$['tool']['jsonpath']['creator']['name']");
         assertEquals("Jayway Inc.", jsonpathCreatorName);
+        //
+        List<String> jsonpathCreatorLocation =
+                jsonContext.read("$['tool']['jsonpath']['creator']['location'][*]");
         assertTrue(jsonpathCreatorLocation.toString().contains("Malmo"));
+        assertTrue(jsonpathCreatorLocation.toString().contains("San Francisco"));
+        assertTrue(jsonpathCreatorLocation.toString().contains("Helsingborg"));
+    }
+
+    @Test
+    public void test4_2_Predicates() throws IOException {
+        Path dataFile = Paths.get("./src/test/fixture/4_2_Predicates.json");
+        String jsonDataSourceString = readData(dataFile);
+        Filter expensiveFilter = Filter.filter(Criteria.where("price").gt(20.00));
+        List<Map<String,Object>> expensive =
+                JsonPath.parse(jsonDataSourceString)
+                        .read("$['book'][?]", expensiveFilter);
+        predicateUsageAssertionHelper(expensive);
+        //
+        expensive =
+                JsonPath.parse(jsonDataSourceString)
+                        .read("$['book'][?(@['price'] > $['price range']['medium'])]");
+        predicateUsageAssertionHelper(expensive);
+    }
+
+    private void predicateUsageAssertionHelper(List<?> predicate) {
+        assertTrue(predicate.toString().contains("Beginning JSON"));
+        assertTrue(predicate.toString().contains("JSON at Work"));
+        assertFalse(predicate.toString().contains("Learn JSON in a DAY"));
+        assertFalse(predicate.toString().contains("JSON: Questions and Answers"));
+
     }
 }
